6a9faf71b1d5502f1f0dc0b5c142075483914061
ravikloc <56706112+ravikloc@users.noreply.github.com>
2020-01-02 15:52:15 +0530

    Set a minimum order amount for checkout

--- minimum-order-amount.php
+++ minimum-order-amount.php
@@ -1,10 +1,33 @@
-<?php 
+/**
+ * Set a minimum order amount for checkout
+ */
 add_action( 'woocommerce_checkout_process', 'wc_minimum_order_amount' );
+add_action( 'woocommerce_before_cart' , 'wc_minimum_order_amount' );
+ 
 function wc_minimum_order_amount() {
-	global $woocommerce;
-	$minimum = 50;
-	if ( $woocommerce->cart->get_cart_total(); < $minimum ) {
-           $woocommerce->add_error( sprintf( 'You must have an order with a minimum of %s to place your order.' , $minimum ) );
-	}
+    // Set this variable to specify a minimum order value
+    $minimum = 50;
+
+    if ( WC()->cart->total < $minimum ) {
+
+        if( is_cart() ) {
+
+            wc_print_notice( 
+                sprintf( 'Your current order total is %s — you must have an order with a minimum of %s to place your order ' , 
+                    wc_price( WC()->cart->total ), 
+                    wc_price( $minimum )
+                ), 'error' 
+            );
+
+        } else {
+
+            wc_add_notice( 
+                sprintf( 'Your current order total is %s — you must have an order with a minimum of %s to place your order' , 
+                    wc_price( WC()->cart->total ), 
+                    wc_price( $minimum )
+                ), 'error' 
+            );
+
+        }
+    }
 }
-?>
